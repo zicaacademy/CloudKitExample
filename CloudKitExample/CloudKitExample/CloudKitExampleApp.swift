ece3a999edfea3bf9aa453f946d07d7503f15457
zicaacademy <138627459+zicaacademy@users.noreply.github.com>
2024-12-10 14:52:54 -0300

    adding files

---
+++ CloudKitExample/CloudKitExample/CloudKitExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudKitExampleApp.swift
+//  CloudKitExample
+//
+//  Created by Luiz Felipe on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct CloudKitExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
